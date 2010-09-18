b90025802b79ccb85e981def4fca473b9394eda6
Thorben Schröder <stillepost@gmail.com>
2010-09-18 16:43:15 +0200

    Initial commit.

---
+++ bootstrap.rb
@@ -0,0 +1,5 @@
+ROOT_PATH = File.expand_path("#{File.dirname(__FILE__)}/")
+
+# Initialize the beefdump system
+require 'beefdump'
+require 'beefdump/boot'
---
+++ start.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+
+require 'bootstrap'
+
+#Start game
+Beefdump::Game::Base.new
